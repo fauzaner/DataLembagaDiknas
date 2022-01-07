4761a19b340af2927bb18bfd08460acda6e1dafc
ayuariesta <=>
2022-01-07 11:05:33 +0700

    perubahan isi halaman pd

--- resources/views/daftar_pd.blade.php
+++ resources/views/daftar_pd.blade.php
@@ -1,3 +1,19 @@
 @extends('main')
 @section('content')
+<table class="table table-bordered table-striped table-hover">
+    <thead class="table-light">
+        <th>Nomor Surat</th>
+        <th>Kategori</th>
+        <th>Judul</th>
+        <th>Waktu Pengarsipan</th>
+        <th>Aksi</th>
+    </thead>
+    <tbody>
+        <td>rte</td>
+        <td>ert</td>
+        <td>rtr</td>
+        <td>reg</td>
+        <td>th</td>
+    </tbody>
+</table>
 @endsection
