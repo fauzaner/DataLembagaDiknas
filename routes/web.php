10ac6ddeb6dbb9eec5d0a65108712f094eb76d17
ayuariesta <=>
2022-01-11 11:38:15 +0700

    ubahan ayu di database dan route

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ use App\Models\Guru;
 Route::get('/', function () {return view('sup-admin'); });
 Route::get('/form', function () { return view('form'); });
 Route::get('/upload', function () { return view('upload'); });
-
 Route::get('/pd', function () { $peserta_didik = PesertaDidik::all(); 
     return view('daftar_pd',['peserta_didik'=>$peserta_didik]); });
 Route::post('/pd', function () { Excel::import(new PDImport, request()->file('file')); 
@@ -27,5 +26,4 @@ Route::post('/pd', function () { Excel::import(new PDImport, request()->file('fi
     return back(); });
 Route::get('/login', function () {return view('login'); });
 Route::get('/admin', function () {return view('admin'); });
-Route::post('/pd/', function () { Excel::import(new PDImport, request()->file('file')); return back(); });
 Route::get('/guru', function () { $guru = Guru::all(); return view('daftar_guru',['guru'=>$guru]); });
