69658aef6516addc52031ad81081c9d9971a060c
fauzaner <fauzankertanegara@gmail.com>
2022-01-24 13:46:05 +0700

    nyoba

--- routes/web.php
+++ routes/web.php
@@ -8,24 +8,12 @@ use App\Models\Guru;
 use App\Models\Tendik;
 use App\Http\Controllers\adminController;
 use App\Http\Controllers\cobaController;
-use App\Http\Controllers\LoginController;
 use App\Models\Users;
-<<<<<<< HEAD
 use App\Http\Controllers\TestController;
 use App\Http\Conttollers\dashboardController;
-
-
-=======
 use App\Http\Controllers\loginController;
-use App\Http\Controllers\AdminController;
-<<<<<<< HEAD
 use App\Http\Controllers\DataPDController;
 use App\Http\Controllers\ProfilControlle;
-
-=======
->>>>>>> faaf645b0a82765e06baa19fc2e942f58f5245bd
->>>>>>> c94bd857787b236e53e34fde87dde5928aabe732
->>>>>>> 1342c284e96688b384acb1e38f81cd7f1ddbf0e6
 
 
 /*
@@ -41,10 +29,6 @@ use App\Http\Controllers\ProfilControlle;
 Route::get('/sup_admin', function () {return view('sup_admin'); });
 Route::get('/form', function () { return view('form'); });
 Route::get('/upload', function () { return view('upload'); });
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
 Route::get('/pd', [App\Http\Controllers\DataPDController::class, 'index'])->name('daftar_pd');
 Route::post('/pd/import', [App\Http\Controllers\DataPDController::class, 'import']);
 Route::get('/profil', [App\Http\Controllers\ProfilController::class, 'index'])->name('daftar_profil');
@@ -52,60 +36,43 @@ Route::post('/profil/import', [App\Http\Controllers\ProfilController::class, 'im
 Route::get('/admin', function () {return view('admin'); });
 Route::get('/sup_admin', function () {return view('sup_admin'); });
 Route::get('/guru', function () { $guru = Guru::all(); return view('daftar_guru',['guru'=>$guru]); });
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 1342c284e96688b384acb1e38f81cd7f1ddbf0e6
 Route::get('/pd', function () { $peserta_didik = PesertaDidik::all();
     return view('daftar_pd',['peserta_didik'=>$peserta_didik]);
 });
 Route::get('/coba', function () {return view('coba'); });
-<<<<<<< HEAD
+
 
 Route::get('/pd', [App\Http\Controllers\DataPDController::class, 'index']);
 Route::post('/pd/import', [App\Http\Controllers\DataPDController::class, 'import']);
 
-=======
-=======
-<<<<<<< HEAD
->>>>>>> e77cc47839e53ccb0cceb8675dd52c2ccd381810
 Route::get('/pd', [App\Http\Controllers\DataPDController::class, 'index']);
 Route::post('/pd/import', [App\Http\Controllers\DataPDController::class, 'import']);
->>>>>>> 1342c284e96688b384acb1e38f81cd7f1ddbf0e6
 Route::get('/pd', function () { $peserta_didik = PesertaDidik::all();return view('admin.daftar_pd',['peserta_didik'=>$peserta_didik]);});
 
 Route::post('/pd', function () { Excel::import(new PDImport, request()->file('file'));
     Alert::success('Congrats', 'You\'ve Successfully Registered');
     return back(); });
-<<<<<<< HEAD
+
 Route::get('/user', function () {return view('sup_admin.tambah_user'); });
 Route::get('/user', function () { $users = Users::all();return view('sup_admin.tambah_user',['users'=>$users]);});
 Route::get('/admin', function () {return view('admin'); });
-=======
 Route::get('/user', [App\Http\Controllers\UserController::class, 'user']);
 Route::get('/user', function () { $users = Users::all();return view('sup_admin.tambah_user',['users'=>$users]);});
-
->>>>>>> 1342c284e96688b384acb1e38f81cd7f1ddbf0e6
 Route::get('/sup_admin', function () {return view('sup_admin'); });
->>>>>>> c94bd857787b236e53e34fde87dde5928aabe732
+
 
 //Login Routes
-<<<<<<< HEAD
+
 
 Auth::routes();
 
 Route::get('/login', [loginController::class, 'login']);
-=======
 Auth::routes();
 
 Route::get('/login', [loginController::class, 'login']);
-<<<<<<< HEAD
-=======
 
->>>>>>> c94bd857787b236e53e34fde87dde5928aabe732
+
 Auth::routes(); 
->>>>>>> 1342c284e96688b384acb1e38f81cd7f1ddbf0e6
 
 
 
@@ -115,20 +82,11 @@ Route::get('/', [loginController::class, 'login'])->name('login')->middleware('g
 Route::post('/', [loginController::class, 'authenticate']);
 
 Route::get('/admin', [AdminController::class, 'admin']);
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
+
+
 Route::get('/tk', function () { $tendik = Tendik::all(); return view('daftar_tendik',['tendik'=>$tendik]); });
-=======
-<<<<<<< HEAD
-=======
->>>>>>> f1913a50d1a9783c21d1d2b5deb7ca80d72c0720
->>>>>>> faaf645b0a82765e06baa19fc2e942f58f5245bd
->>>>>>> e77cc47839e53ccb0cceb8675dd52c2ccd381810
->>>>>>> 1342c284e96688b384acb1e38f81cd7f1ddbf0e6
 
->>>>>>> c94bd857787b236e53e34fde87dde5928aabe732
 Route::get('/td', function () { $tendik = Tendik::all(); return view('daftar_tendik',['tendik'=>$tendik]); });
 Route::get('/guru', function () { $guru = Guru::all(); return view('daftar_guru',['guru'=>$guru]); });
 
