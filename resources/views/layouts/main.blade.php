cbe550e959c2547d0f650f2d58419670447b2e19
ayuariesta <=>
2022-01-31 07:18:33 +0700

    mengubah main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,11 +2,13 @@
 
 <html lang="en" dir="ltr">
   <head>
+    <title>Data Warehouse Dinas Pendidikan</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset('assets/css/admins.css')}}">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="{{asset('css/app.css')}}" />
+    <link rel="shortcut-icon" href="{{asset('img/Logo.png')}}">
    </head>
 <body>
   @include('sweetalert::alert')
@@ -17,23 +19,23 @@
     </div>
     <ul class="nav-list">
       <li>
-          <i class='bx bx-search' ></i>
-         <input type="text" placeholder="Search...">
-         <span class="tooltip">Search</span>
-      </li>
-      <li>
-        <a href="{{url('admin')}}">
+        <a href="{{route('dashboard_admin')}}">
           <i class='bx bx-grid-alt'></i>
           <span class="links_name">Dashboard</span>
         </a>
          <span class="tooltip">Dashboard</span>
       </li>
       <li>
+          <i class='bx bx-search' ></i>
+         <input type="text" placeholder="Search...">
+         <span class="tooltip">Search</span>
+      </li>
+      <li>
         <a href="#">
-         <i class='bx bxs-download'></i>
-         <span class="links_name">Download</span>
+        <i class='bx bx-data'></i>
+         <span class="links_name">Data Master</span>
         </a>
-       <span class="tooltip">Download</span>
+       <span class="tooltip">Data Master</span>
      </li>
      <li>
        <a href="#">
